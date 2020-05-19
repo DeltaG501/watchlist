ec75da47535467a9b56c1e438f9ad16c63228225
DeltaG501 <tom.te.luo+SignUp@gmail.com>
2020-05-19 12:31:36 +0800

    Add minimal home page

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 
+@app.route('/')
+def hello():
+    return 'Hello'
+
+
 @app.route('/user/<name>')
-def hello(name):
-    return "<h1>Welcome to %s's watchlist</h1>" % name
+def user_page(name):
+    return 'User: %s' % name
+
+
+@app.route('/test')
+def test_url_for():
+    print(url_for('hello'))
+    print(url_for('user_page', name='Tom'))
+    print(url_for('test_url_for'))
+    print(url_for('test_url_for', num=2))
+    return 'Test Page'
